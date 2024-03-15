a73a09fd983d8e089c95e4e2f28cdfab9bda50b9
Oleksandr Boiko <boiko20009@gmail.com>
2024-03-15 11:36:41 +0200

    1 try

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    // Додати атрибут data-modal на бекдроп модалки
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.menu.classList.toggle("is-open");
+  }
+  
+})();
